850a201a5c7323d9638c7f5dc3630597e08f04c8
queirozdaniel <danielqueirozak77@gmail.com>
2020-06-08 23:59:05 -0300

    Adicionando API falsa e criando pagina inicial

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const vm = new Vue({
     el: "#app",
     data: {
-        mensagem: "ola"
+        produtos: []
+    },
+    methods: {
+        puxarProdutos() {
+            fetch('./api/produtos.json')
+                .then(r => r.json())
+                .then(r => {
+                    this.produtos = r
+                })
+        }
+    },
+    created() {
+        this.puxarProdutos()
     }
 
 
